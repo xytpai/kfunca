c568dad1afdc11ded2ebf7b4b2dd8f30f4cf1aae
Yutao Xu <xytpai@foxmail.com>
2025-03-03 17:11:49 +0800

    Enable first CUDA kernel (#2)

--- src/register.cpp
+++ src/register.cpp
@@ -1,30 +1,11 @@
 #include <pybind11/pybind11.h>
 #include <pybind11/numpy.h>
 #include <iostream>
+#include "device_info.h"
 
 namespace py = pybind11;
 
-template <typename T>
-void process_numpy_array(py::array_t<T> arr) {
-    std::cout << sizeof(T) << "\n";
-    // 获取数据指针
-    py::buffer_info buf_info = arr.request();
-    double *ptr = (double *)buf_info.ptr;
-
-    // 获取数组的形状
-    size_t rows = buf_info.shape[0];
-    size_t cols = buf_info.shape[1];
-
-    // 打印数组的内容
-    for (size_t i = 0; i < rows; ++i) {
-        for (size_t j = 0; j < cols; ++j) {
-            std::cout << ptr[i * cols + j] << " ";
-        }
-        std::cout << std::endl;
-    }
-}
-
 PYBIND11_MODULE(kfunca, m) {
-    m.def("process_numpy_array", &process_numpy_array<double>, "Process a numpy array");
-    m.def("process_numpy_array", &process_numpy_array<float>, "Process a numpy array");
+    m.def("device_property", &device_property, "Get device info");
+    m.def("global_memory_bandwidth", &global_memory_bandwidth, "Get global_memory_bandwidth");
 }
