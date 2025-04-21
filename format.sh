c7be43ff2d4187b26815c37c6a521d3e763055c2
xytpai <xytpai@gmail.com>
2025-04-21 23:29:43 +0800

    add cpp test

--- format.sh
+++ format.sh
@@ -1,1 +1,2 @@
 find ./src \( -name "*.h" -o -name "*.cpp" -o -name "*.cu" \) -exec clang-format -i {} +
+find ./test/cpp \( -name "*.h" -o -name "*.cpp" -o -name "*.cu" \) -exec clang-format -i {} +
---
+++ test/cpp/test_tensor.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+#include <cassert>
+
+#include "tensor.h"
+
+int main() {
+    std::vector<int64_t> shape = {3, 5};
+    int tensor_data[15] = {
+        3, 2, -1, 2, 3, // 0
+        5, 4, 3, 4, 5,  // 1
+        2, 4, 6, 8, 6,  // 2
+    };
+    auto t = empty(shape, ScalarType::Int, 0);
+    t.copy_from_cpu_ptr(tensor_data);
+    t = t + t;
+    int out[15] = {0};
+    t.copy_to_cpu_ptr(out);
+    for (int i = 0; i < 15; i++) {
+        assert(tensor_data[i] == out[i] / 2);
+    }
+}
