80d3268928022f0949192ef70088daa7ced65b26
xytpai <xytpai@gmail.com>
2025-04-21 23:41:04 +0800

    for ci test

--- test/cpp/test_tensor.cpp
+++ test/cpp/test_tensor.cpp
@@ -18,6 +18,6 @@ int main() {
     int out[15] = {0};
     t.copy_to_cpu_ptr(out);
     for (int i = 0; i < 15; i++) {
-        assert(tensor_data[i] == out[i] / 2);
+        assert(tensor_data[i] == out[i]);
     }
 }
